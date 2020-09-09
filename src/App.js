ad7b95aa620038fd20ebef5233d90dfa00f041e6
danielHollander <70101641+danielHollander@users.noreply.github.com>
2020-09-09 14:13:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Data from './features/counter/data'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      "Hello world"
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/counter/data.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Data = [
+    {
+        id: 1,
+        website: "www.example.com",
+        actions: true,
+        tag: true,
+        implementaion: "Hardcoded",
+        cmp: false,
+        pageViews: 1000,
+        percentagePC: 80,
+        percentageTablet: 5,
+        percentageMobile: 15,
+        totalPlacements: 10,
+        requests: 100000,
+        imp: 90000,
+        revenue: 1000
+
+    }
+
+]
